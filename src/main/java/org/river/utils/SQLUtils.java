e193c66fe3fadfb7088a1c5c9b942c21b9d1a2eb
RenKaiZheng <43511153+RenKaiZheng@users.noreply.github.com>
2020-06-03 01:43:55 +0800

    Second version re-update
    河河

--- src/main/java/org/river/utils/SQLUtils.java
+++ src/main/java/org/river/utils/SQLUtils.java
@@ -4,6 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import org.river.entities.Restaurant;
+import org.river.exceptions.QueryException;
+import org.river.models.DBConnect;
+
 import javafx.scene.image.Image;
 
 public class SQLUtils {
@@ -35,6 +39,27 @@ public class SQLUtils {
 	        out += (buffer[i + offset] & 0x000000FF) << shift;
 	    }
 		return out;
+	}
+
+	public static Restaurant queryRestaurant(int id) throws QueryException {
+		DBConnect DBC = new DBConnect();
+    	Connection con = DBC.getConnect();
+    	
+    	try {
+    		String sqlQuery = "select * from restaurant where id=?";
+    		PreparedStatement stat = con.prepareStatement(sqlQuery);
+    		stat.setInt(1, id);
+    		ResultSet rs = stat.executeQuery();
+    		
+    		while (rs.next())
+	    		return new Restaurant(id, rs.getInt("area_id"), 
+		    				rs.getInt("food_category_id"),	rs.getString("name"), 
+		    				rs.getString("description"), ImageUtils.BlobToImage(rs.getBlob("image")), 
+		        			rs.getString("address"));
+    	} catch (Exception r) {
+    		throw new QueryException("queryRestaurant error: utils");
+    	}
+    	throw new QueryException("queryRestaurant error: utils");
 	}
 
 }
