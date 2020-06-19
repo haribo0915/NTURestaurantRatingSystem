fa409be41113a238b104e172eca1c7a691de5879
RenKaiZheng <43511153+RenKaiZheng@users.noreply.github.com>
2020-06-19 15:23:00 +0800

    Connection Pooling is fulfilled

--- src/main/java/org/river/models/DBConnect.java
+++ src/main/java/org/river/models/DBConnect.java
@@ -4,11 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import org.river.exceptions.*;
 
 public class DBConnect {
-	private Connection con;
-	private Statement st;
-	private ResultSet rs;
+	private static Connection con = null;
 	
 	public DBConnect() {
 		try {
@@ -16,15 +15,21 @@ public class DBConnect {
 			con = 
 			DriverManager.getConnection("jdbc:mysql://localhost:3306/ntu_restaurant_rating_system?serverTimezone=UTC"
 					, "root", "root");
-			st = con.createStatement();
 		}catch (Exception e) {}
 	}
 	
-	public Connection getConnect() {
-		return con;
+	public static Connection getConnect() {
+		try {
+			if (con == null) {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ntu_restaurant_rating_system?serverTimezone=UTC"
+								, "root", "root");
+				return con;
+			}
+			else
+				return con;
+		} catch (Exception e) {}
+		return null;
 	}
 	
-	public Statement getStatement() {
-		return st;
-	}
 }
